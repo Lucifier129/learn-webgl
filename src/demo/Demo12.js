ead92e58db21c8f45c9ebd34726a90b97f6cb6a8
yj_gu <yj_gu@ctrip.com>
2019-07-08 16:13:15 +0800

    update

--- src/demo/Demo12.js
+++ src/demo/Demo12.js
@@ -19,7 +19,7 @@ const getMSE = (listA, listB) => {
 }
 
 // mean squared error
-const sortByMSE = (prev, current, renderCount) => {
+const sortByMSE = (prev, current) => {
   let result = []
 
   for (let i = 0; i < prev.length; i += 4) {
@@ -29,7 +29,7 @@ const sortByMSE = (prev, current, renderCount) => {
 
     result[index] = {
       index,
-      value: getMSE(listA, listB) / renderCount[i]
+      value: getMSE(listA, listB)
     }
   }
 
@@ -44,16 +44,19 @@ export default function Demo01() {
   let [count, setCount] = useState(0)
   let [time, setTime] = useState(0)
   let ref = useRef()
+  let deubgRef = useRef()
   let width = 800
   let height = 400
 
   useEffect(() => {
     let canvas = ref.current
+    let debugCanvas = deubgRef.current
     let ctx = canvas.getContext('2d')
     let ray = createRayTracing({ width, height, amount: 50 })
     let imageData = ctx.createImageData(width, height)
     let data = new Float32Array(width * height * 4)
-    let cache = new Float32Array(width * height * 4)
+    let prevImageData = new Float32Array(width * height * 4)
+    let currImageData = new Float32Array(width * height * 4)
     let renderCount = new Float32Array(width * height * 4)
     let innerCount = 1
     let innerTime = 0
@@ -65,6 +68,52 @@ export default function Demo01() {
       ctx.putImageData(imageData, 0, 0)
     }
 
+    let showData = () => {
+      let data = renderCount
+      let ctx = debugCanvas.getContext('2d')
+      let imageData = ctx.createImageData(width, height)
+
+      for (let i = 0; i < data.length; i++) {
+        if ((i + 1) % 4 === 0) {
+          imageData.data[i] = 255
+        } else {
+          imageData.data[i] = data[i]
+        }
+      }
+      ctx.clearRect(0, 0, width, height)
+      ctx.putImageData(imageData, 0, 0)
+    }
+
+    let renderByPosition = (x, y) => {
+      let [r, g, b, a = 1] = ray.renderByPosition(x, y)
+      let i = ((height - y) * width + x) * 4
+
+      data[i + 0] += r
+      data[i + 1] += g
+      data[i + 2] += b
+      data[i + 3] += a
+
+      renderCount[i + 0] += 1
+      renderCount[i + 1] += 1
+      renderCount[i + 2] += 1
+      renderCount[i + 3] += 1
+
+      prevImageData[i + 0] = imageData.data[i + 0]
+      prevImageData[i + 1] = imageData.data[i + 1]
+      prevImageData[i + 2] = imageData.data[i + 2]
+      prevImageData[i + 3] = imageData.data[i + 3]
+
+      imageData.data[i + 0] = toColor(data[i + 0] / renderCount[i + 0])
+      imageData.data[i + 1] = toColor(data[i + 1] / renderCount[i + 1])
+      imageData.data[i + 2] = toColor(data[i + 2] / renderCount[i + 2])
+      imageData.data[i + 3] = toColor(data[i + 3] / renderCount[i + 3])
+
+      currImageData[i + 0] = imageData.data[i + 0]
+      currImageData[i + 1] = imageData.data[i + 1]
+      currImageData[i + 2] = imageData.data[i + 2]
+      currImageData[i + 3] = imageData.data[i + 3]
+    }
+
     let render = async () => {
       let start = Date.now()
       let i = 0
@@ -73,13 +122,17 @@ export default function Demo01() {
       for (let n of ray.render()) {
         renderCount[i] += 1
         data[i] += n
-        cache[i] = imageData.data[i]
+        prevImageData[i] = imageData.data[i]
         imageData.data[i] = toColor(data[i] / renderCount[i])
+        currImageData[i] = imageData.data[i]
         i += 1
         duration = Date.now() - start
         if (duration % 100 === 0) {
           setTime(innerTime + duration)
-          await frame(renderToCanvas)
+          await frame(() => {
+            renderToCanvas()
+            showData()
+          })
         }
         if (over) return
       }
@@ -98,52 +151,25 @@ export default function Demo01() {
       }
     }
 
-    let scheduleRender = async () => {
-      let list = sortByMSE(cache, imageData.data, renderCount)
+    let scheduleRender = async (count = 0) => {
+      let list = sortByMSE(prevImageData, currImageData)
       let start = Date.now()
       let duration = 0
 
-      for (let i = 0; i < 100000; i++) {
+      for (let i = 0; i < 20000; i++) {
         let item = list[i]
-        let index = item.index * 4
         let x = item.index % width
         let y = height - Math.floor(item.index / width)
-        let [r, g, b, a = 1] = ray.renderByPosition(x, y)
-
-        let rIndex = index + 0
-        let gIndex = index + 1
-        let bIndex = index + 2
-        let aIndex = index + 3
-
-        renderCount[rIndex] += 1
-        renderCount[gIndex] += 1
-        renderCount[bIndex] += 1
-        renderCount[aIndex] += 1
-
-        data[rIndex] += r
-        data[gIndex] += g
-        data[bIndex] += b
-        data[aIndex] += a
-
-        cache[rIndex] = imageData.data[rIndex]
-        cache[gIndex] = imageData.data[gIndex]
-        cache[bIndex] = imageData.data[bIndex]
-        cache[aIndex] = imageData.data[aIndex]
-
-        imageData.data[rIndex] = toColor(data[rIndex] / renderCount[rIndex])
-        imageData.data[gIndex] = toColor(data[gIndex] / renderCount[gIndex])
-        imageData.data[bIndex] = toColor(data[bIndex] / renderCount[bIndex])
-        imageData.data[aIndex] = toColor(data[aIndex] - 1 / renderCount[aIndex])
-
+        
+        renderByPosition(x, y)
         duration = Date.now() - start
 
-        if (duration > 20000) {
-          return render()
-        }
-        
         if (duration % 100 === 0) {
           setTime(innerTime + duration)
-          await frame(renderToCanvas)
+          await frame(() => {
+            renderToCanvas()
+            showData()
+          })
         }
         if (over) return
       }
@@ -153,7 +179,13 @@ export default function Demo01() {
       renderToCanvas()
       setCount(innerCount)
       setTime((innerTime += Date.now() - start))
-      tid = requestAnimationFrame(scheduleRender)
+      tid = requestAnimationFrame(() => {
+        if (count < 5) {
+          scheduleRender(count + 1)
+        } else {
+          render()
+        }
+      })
     }
 
     render()
@@ -167,6 +199,7 @@ export default function Demo01() {
   return (
     <>
       <canvas width={width} height={height} ref={ref} />
+      <canvas width={width} height={height} ref={deubgRef} />
       <h3>光线追踪时间：{(time / 1000).toFixed(2)}秒</h3>
       <h3>渲染次数：{count}</h3>
       <h3>平均时间：{(count ? time / 1000 / count : 0).toFixed(2)}秒</h3>
